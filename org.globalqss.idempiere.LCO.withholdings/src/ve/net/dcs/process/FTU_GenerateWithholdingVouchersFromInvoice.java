3045abea21ee0c3a134b97766a96f15d6810f7e6
Jorge Colmenarez <jlct.master@gmail.com>
2020-11-25 17:02:05 -0400

    Clean code and prevent excesive transaction to database.

--- org.globalqss.idempiere.LCO.withholdings/src/ve/net/dcs/process/FTU_GenerateWithholdingVouchersFromInvoice.java
+++ org.globalqss.idempiere.LCO.withholdings/src/ve/net/dcs/process/FTU_GenerateWithholdingVouchersFromInvoice.java
@@ -6,14 +6,12 @@ import java.sql.Timestamp;
 import java.util.List;
 
 import org.adempiere.exceptions.AdempiereException;
-import org.compiere.model.MInvoice;
 import org.compiere.model.Query;
 import org.compiere.process.ProcessInfoParameter;
 import org.compiere.process.SvrProcess;
 import org.compiere.util.DB;
 import org.compiere.util.Msg;
 import org.globalqss.model.MLCOInvoiceWithholding;
-import org.globalqss.model.MLCOWithholdingType;
 import org.globalqss.model.X_LCO_InvoiceWithholding;
 
 import ve.net.dcs.model.MLVEVoucherWithholding;
@@ -59,10 +57,6 @@ public class FTU_GenerateWithholdingVouchersFromInvoice extends SvrProcess{
 
 		String msg = Msg.parseTranslation(getCtx(), "@LVE_VoucherWithholding_ID@");
 		
-		//MLCOWithholdingType WithholdingType = new MLCOWithholdingType(getCtx(), withholdingType, get_TrxName());
-		
-		/*String sql = "SELECT inv.C_BPartner_ID, inv.AD_Org_ID FROM T_Selection ts JOIN C_Invoice inv ON (ts.ViewID)::numeric = inv.C_Invoice_ID"
-				+ " WHERE ts.AD_PInstance_ID="+getAD_PInstance_ID()+" GROUP BY inv.C_BPartner_ID, inv.AD_Org_ID";*/
 		String sql = "SELECT src.C_Invoice_ID,src.lco_withholdingtype_id FROM FTU_WithholdingVoucherSrc src "
 				+ " WHERE EXISTS (SELECT 1 FROM T_Selection ts WHERE trim(ts.ViewID)::numeric = src.C_Invoice_ID AND ts.AD_PInstance_ID="+getAD_PInstance_ID()+")"
 				+ " ORDER BY src.C_BPartner_ID, src.AD_Org_ID";
@@ -74,44 +68,6 @@ public class FTU_GenerateWithholdingVouchersFromInvoice extends SvrProcess{
 			
 			pstmt= DB.prepareStatement(sql, get_TrxName());
 			rs =  pstmt.executeQuery();
-			/*int ins = 0;
-			while(rs.next()) {
-				int C_BPartner_ID = rs.getInt(1);
-				int AD_Org_ID = rs.getInt(2);
-				if(!(C_BPartner_ID>0)) 
-					continue;
-				MLVEVoucherWithholding voucher = new MLVEVoucherWithholding(getCtx(), 0, get_TrxName());;
-				voucher.setAD_Org_ID(AD_Org_ID);
-				voucher.setC_BPartner_ID(C_BPartner_ID);
-				voucher.setDateTrx(dateTrx);
-				voucher.set_ValueOfColumn("DateAcct",dateTrx);
-				voucher.setLCO_WithholdingType_ID(withholdingType);
-				voucher.setIsSOTrx(WithholdingType.isSOTrx());
-				voucher.setC_Currency_ID(currencyId);
-				voucher.setC_ConversionType_ID(conversiontypeId);
-				voucher.saveEx(get_TrxName());
-				ins = 0;
-				for (MInvoice invoice : getSelectedInvoiceOfBPartner(C_BPartner_ID,AD_Org_ID)) {
-					VWT_MInvoice inv = new VWT_MInvoice(getCtx(), invoice.getC_Invoice_ID(), get_TrxName());	
-					  int cont = inv.recalcWithholdings(voucher);
-					  ins += cont;
-				}
-				if(ins>0) {
-					if(docAction.equals("CO"))
-						processWithholding(voucher);
-					
-					voucher.saveEx(get_TrxName());
-					String WithholdingNo = DB.getSQLValueString(get_TrxName(), "SELECT WithholdingNo FROM LVE_VoucherWithholding WHERE LVE_VoucherWithholding_ID=?", voucher.get_ID());
-					//String WithholdingNo = voucher.getWithholdingNo();
-					addBufferLog(voucher.get_ID(), new Timestamp(System.currentTimeMillis()), null, msg+": "+WithholdingNo, voucher.get_Table_ID(), voucher.get_ID());
-					cnt += ins;
-				}else {
-					voucher.deleteEx(true, get_TrxName());
-				}
-				
-				
-			}*/
-			
 			
 			int ins = 0;
 			int OldBPartner_ID = 0;
@@ -132,8 +88,7 @@ public class FTU_GenerateWithholdingVouchersFromInvoice extends SvrProcess{
 								processWithholding(voucher);
 							
 							voucher.saveEx(get_TrxName());
-							String WithholdingNo = DB.getSQLValueString(get_TrxName(), "SELECT WithholdingNo FROM LVE_VoucherWithholding WHERE LVE_VoucherWithholding_ID=?", voucher.get_ID());
-							addBufferLog(voucher.get_ID(), new Timestamp(System.currentTimeMillis()), null, msg+": "+WithholdingNo, voucher.get_Table_ID(), voucher.get_ID());
+							addBufferLog(voucher.get_ID(), new Timestamp(System.currentTimeMillis()), null, msg+": "+voucher.getWithholdingNo(), voucher.get_Table_ID(), voucher.get_ID());
 							cnt += ins;
 						}else {
 							if(voucher.get_ID()>0)
@@ -170,17 +125,6 @@ public class FTU_GenerateWithholdingVouchersFromInvoice extends SvrProcess{
 	
 		
 		return "Retenciones Generadas: "+cnt;
-	}
-	
-	private MInvoice[] getSelectedInvoiceOfBPartner(int C_BPartner_ID,int AD_Org_ID) {
-		String whereClause = "C_Invoice_ID IN ("
-				+ " SELECT inv.C_Invoice_ID FROM T_Selection ts JOIN C_Invoice inv ON (ts.ViewID)::numeric = inv.C_Invoice_ID"
-				+ " WHERE ts.AD_PInstance_ID="+getAD_PInstance_ID()+" AND inv.C_BPartner_ID="+C_BPartner_ID+" AND AD_Org_ID="+AD_Org_ID+" )";
-		List<MInvoice> list = new Query(getCtx(), MInvoice.Table_Name, whereClause, get_TrxName())
-				//.setParameters()
-				//.setOrderBy(FTUMProductionLine.COLUMNNAME_Line)
-				.list();
-				return list.toArray(new MInvoice[list.size()]);
 	}
 	
 	private void processWithholding(MLVEVoucherWithholding voucher)
